d5da4f021a3ce5793b694503a8acd6d4cd163d1e
Michel Zahnd <michel.zahnd123@gmail.com>
2021-11-23 21:59:52 +0100

    encore un petit essai

--- AMardiCpp/lib/gecko/gecko.cpp
+++ AMardiCpp/lib/gecko/gecko.cpp
@@ -1,9 +1,6 @@
 #include <Arduino.h>
 #include "gecko.h"
 
-Gecko::Gecko(arthur) {};             /* constructeur */
-Gecko::Gecko(benji) {};
-
-void afficher(){
-   Serial.println("Hello!");       /* Hello! RetourLigne */ 
+Gecko::Gecko patate {
+   Serial.println("Hello!\n");
 }
--- AMardiCpp/lib/gecko/gecko.h
+++ AMardiCpp/lib/gecko/gecko.h
@@ -1,11 +1,6 @@
 #ifndef GECKO_H
 #define GECKO_H
 
-class Gecko{
-private:
-    char* nom_gecko;
-public:
-    void afficher();
-};
+class Gecko{;};
 
 #endif
