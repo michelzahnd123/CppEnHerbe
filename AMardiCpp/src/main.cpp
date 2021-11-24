c29164cbe9ef7c591e4d10e618cf3f721200a221
Clément Boesmier <cle.chec@laposte.net>
2021-11-24 19:22:17 +0100

    ajout test exercice 2

--- AMardiCpp/src/main.cpp
+++ AMardiCpp/src/main.cpp
@@ -2,23 +2,17 @@
 #include "gecko.h"
 
 void setup() {
-  Serial.begin(115200);             // initialisation du moniteur
+  Serial.begin(115200);
 
-  Serial.println("\nEx1:");         // fin exercice 1 (Clément)
-/*
-  Gecko arthur;                     // instance de Gecko nomée arthur (c'est le nom de ma variable)
-    //le constructeur qui est appele pour la creer affiche hello sur le moniteur serie
-  Gecko benjy;                      // c'est pareil pour benjy (Clément)
-*/
-  Serial.println("\nEx2:");         // au début de chaque exercice pour voir mieux ou l'on se trouve dans la sortie série
-  
-  //String nom_gecko = "arthur";
-  Gecko arthur(String name);         // instance de Gecko nomée arthur
-                                     //le constructeur affiche hello + name
-  Gecko benjy(String name);          // benjy
+  Serial.println("\nEx1:");
+  Gecko arthur;
+  Gecko benjy;
+
+  Serial.println("\nEx2:");
+  Gecko arthur2("test");//on donne le nom test au constructeur de arthur2
 
 
-  Serial.println("\nEx3:");         // au début de chaque exercice
+  Serial.println("\nEx3:");
 }
 
 void loop() {
