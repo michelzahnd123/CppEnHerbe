ae91a5ba6c9a1d1c090458dcc9a7ece099047886
Clément Boesmier <cle.chec@laposte.net>
2021-11-24 12:11:02 +0100

    fix ex01

--- AMardiCpp/src/main.cpp
+++ AMardiCpp/src/main.cpp
@@ -1,21 +1,21 @@
 #include <Arduino.h>
-#include "Gecko.h"//correction clement : impotation de Gecko.h
+#include "gecko.h"//correction clement : impotation de Gecko.h
 
 void setup() {
-    Serial.begin(115200);             // initialisation du moniteur
+  Serial.begin(115200);             // initialisation du moniteur
 
-    Serial.println("\nEx1:");         // fin exercice 1 par clément
-    
-    Gecko arthur();                   // instance de Gecko nomée arthur (c'est le nom de ma variable)
-      //le constructeur qui est appele pour la creer affiche hello sur le moniteur serie
-    Gecko benjy();                    // c'est pareil pour benjy
-    
+  Serial.println("\nEx1:");         // fin exercice 1 par clément
 
-    Serial.println("\nEx2:");         // au début de chaque exercice pour voir mieux ou l'on se trouve dans la sortie série
-                                      // \n = retour a la ligne
+  Gecko arthur;                   // instance de Gecko nomée arthur (c'est le nom de ma variable)
+    //le constructeur qui est appele pour la creer affiche hello sur le moniteur serie
+  Gecko benjy;                    // c'est pareil pour benjy
+
+
+  Serial.println("\nEx2:");         // au début de chaque exercice pour voir mieux ou l'on se trouve dans la sortie série
+                                    // \n = retour a la ligne
 
 }
 
 void loop() {
-                                      // rien a faire en boucle
+  // rien a faire en boucle
 }
