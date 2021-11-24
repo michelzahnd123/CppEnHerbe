e46823c67b6e46a2ea16aa98e6df48ee98d933d2
Michel Zahnd <michel.zahnd123@gmail.com>
2021-11-24 11:52:35 +0100

    enleve commentaire /*

--- AMardiCpp/src/main.cpp
+++ AMardiCpp/src/main.cpp
@@ -1,22 +1,21 @@
 #include <Arduino.h>
-#inlcude "Gecko.h"//correction clement : impotation de Gecko.h
+#include "Gecko.h"//correction clement : impotation de Gecko.h
 
 void setup() {
-    Serial.begin(115200); /* initialisation du moniteur*/
-    Serial.println("\nEx1:")
-    //fin exercice 1 par clément
-    Gecko arthur(); //je crée une instance de Gecko nomée arthur (c'est le nom de ma variable)
-    //le constructeur qui est appeler pour la crée affiche hello sur le moniteur série
-    Gecko benjy(); //c'est pareil pour benjy
+    Serial.begin(115200);             // initialisation du moniteur
 
-    //a vérifier mais je suis pas sur que les "()" sont présente et a leur place faut peut-être les enlever
+    Serial.println("\nEx1:");         // fin exercice 1 par clément
     
+    Gecko arthur();                   // instance de Gecko nomée arthur (c'est le nom de ma variable)
+      //le constructeur qui est appele pour la creer affiche hello sur le moniteur serie
+    Gecko benjy();                    // c'est pareil pour benjy
     
-    Serial.println("\nEx2:")//je coseil de mettre sa au début de chaque exercice pour voir mieux ou l'on se trouve dans la sortie série
-        //\n = retour a la ligne
+
+    Serial.println("\nEx2:");         // au début de chaque exercice pour voir mieux ou l'on se trouve dans la sortie série
+                                      // \n = retour a la ligne
 
 }
 
 void loop() {
-  //rien a faire en boucle
+                                      // rien a faire en boucle
 }
